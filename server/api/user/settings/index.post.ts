e02136daab1c9ea5177c89cae32f90de7a20ef1c
Archameuse <divulinm@gmail.com>
2025-05-23 22:40:06 +0300

    fix avatar upload

--- server/api/user/settings/index.post.ts
+++ server/api/user/settings/index.post.ts
@@ -6,9 +6,9 @@ export default defineEventHandler(async event => {
     const user = readCookie(event)
     if(!user) throw createError({statusCode: 403, message: 'User is not logged in'})
     let {avatar,banner,name,status} = await readBody(event)
-    if(typeof avatar !== 'string') throw createError({statusCode: 400, message: 'Avatar is not a string'})
-    if(typeof banner !== 'string') throw createError({statusCode: 400, message: 'Banner is not a string'})
     if(typeof name !== 'string') throw createError({statusCode: 400, message: 'Username is not a string'})
+    if(typeof avatar !== 'string') avatar = null
+    if(typeof banner !== 'string') banner = null
     if(typeof status !== 'string') status = ''
     try {
         const resp = await db.update(users).set({
